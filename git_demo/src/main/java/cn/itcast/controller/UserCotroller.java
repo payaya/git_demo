97ca95c08f37199ad69d758581574cbe22a7b54d
gaowendong <gaowendong@itcast.cn>
2019-10-26 19:06:56 +0800

    git_demo第一次提交

---
+++ git_demo/src/main/java/cn/itcast/controller/UserCotroller.java
@@ -0,0 +1,12 @@
+package cn.itcast.controller;
+
+import cn.itcast.domain.User;
+
+import java.util.List;
+
+public class UserCotroller {
+
+    public List<User> list(){
+        return null;
+    }
+}
